02f72cc66451788d2c838c898ad58aa914ca0c9f
alberav <agra.alberto@gmail.com>
2019-10-16 14:05:40 +0200

    ejercicios

---
+++ basico03.py
@@ -0,0 +1,6 @@
+nombres = ["Alberto", "Manuel", "Javier"]
+numero = 1
+
+for x in nombres:
+    print("Hola " + str(numero), x)
+    numero = numero + 1
---
+++ basico04.py
@@ -0,0 +1,7 @@
+nombres = ["Maria", "Julia", "Ana", "Miguel", "Pepe", "Popeye"]
+print("Tengo " ,(len(nombres)) + " nombres en mi lista")
+print("Que nombre quieres?")
+x = int(input())
+print(nombres[x])
+#y = nombres[x]
+#print("Has escogido el numero " + str(y) + "que es "+ y)
