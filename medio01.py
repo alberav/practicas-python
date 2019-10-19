b6a8f8d82d1242220dbab1ca63135e4ee32493ab
Alberto <agra.alberto@gmail.com>
2019-10-19 14:21:17 +0200

    Add medio01 DNI

--- medio01.py
+++ medio01.py
@@ -1,11 +1,35 @@
 import re
 
+
+def compruebaEmail(email):
+    if(re.search("(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9.]+$)", email)):
+        return print("Email correcto")
+    else:
+        return print("Email incorrecto")
+
+
+def compruebaDNI(dni):
+
+    # NIE
+    # re.search("^[XxYyZz]{1}\d{7}[a-zA-Z]{1}$", dni)
+
+    letras = ("T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X",
+              "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E")
+
+    comprueba = re.search("^\d{8}[a-zA-Z]{1}$", dni)
+
+    if(comprueba):
+        numeroDNI = (int)(dni[0:8])
+        letraDNI = dni[8]
+        if(letraDNI.upper() == letras[numeroDNI % 23]):
+            return print("DNI correcto")
+        elif(letraDNI.upper() != letras[numeroDNI % 23]):
+            return print("Letra incorrecta")
+    else:
+        return print("DNI incorrecto")
+
+
 email = input("Dirección de correo: ")
-#x = re.search("(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email)
-
-
-if(re.search("(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9.]+$)", email)):
-    print("Email correcto")
-else:
-    print("Email incorrecto")
-
+dni = input("Introduce DNI: ")
+compruebaEmail(email)
+compruebaDNI(dni)
